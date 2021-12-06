927b64436888d494a171f32750e8f3742aa2a129
iavianm <iavianm@mail.ru>
2021-12-06 18:55:27 +0300

    new bbq app

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -9,7 +9,9 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 import '../styles/application.scss'
-const images = require.context('../images', true)
+// const images = require.context('../images', true)
+const images = require.context('../../frontend/images', true)
+const imagePath = (name) => images(name, true)
 
 import 'bootstrap/dist/js/bootstrap'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def user_avatar(user)
+    image_pack_tag('user.png')
+  end
 end
