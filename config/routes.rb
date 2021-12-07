fa41b4722ba1e2ee72f510d8d069d29f56dcc198
iavianm <iavianm@mail.ru>
2021-12-07 16:12:00 +0300

    add subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-
+  
   devise_for :users
   root 'events#index'
   resources :events do
-    resources :comments, only: [:create, :destroy]
+    resources :comments, only: %i[create destroy]
+    resources :subscriptions, only: %i[create destroy]
   end
   resources :users, only: %i[show edit update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
