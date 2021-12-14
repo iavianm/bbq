162166bb943b786e701fe4f97061e9e088ff8a97
iavianm <iavianm@mail.ru>
2021-12-14 23:53:14 +0300

    add region carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,14 +2,14 @@ require 'carrierwave/storage/fog'
 
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
-      region:                'eu-west-2',
+      region:                'us-west-2',
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY']
     }
-
+    
     config.fog_directory  = ENV['S3_BUCKET_NAME']
   end
 end
+
