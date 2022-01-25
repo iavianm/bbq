fdadd5ac64a27299d7e17301fec78aa7206c45ca
iavianm <iavianm@mail.ru>
2022-01-25 23:48:31 +0300

    two refactor mailjet

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,18 +113,18 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   # Базовый урл сайта, для генерации правильных ссылок в письмах
   # ПРОПИСЫВАЙТЕ свой!
-  # config.action_mailer.default_url_options = { host: 'bbqiavianm.herokuapp.com', protocol: 'https' }
-  #
-  # # Вываливать ли посетителю сайта ошибки при отправке писем
-  # config.action_mailer.raise_delivery_errors = false
-  #
-  # # Делать рассылку писем (если false — мэйлер только имитирует работу, реальных писем не уходит)
-  # config.action_mailer.perform_deliveries = true
-  #
-  # # отправка почты по протоколу SMTP
-  # config.action_mailer.delivery_method = :smtp
-  #
-  # # Настройки для Sendgrid
+  config.action_mailer.default_url_options = { host: 'bbqiavianm.herokuapp.com', protocol: 'https' }
+
+  # Вываливать ли посетителю сайта ошибки при отправке писем
+  config.action_mailer.raise_delivery_errors = false
+
+  # Делать рассылку писем (если false — мэйлер только имитирует работу, реальных писем не уходит)
+  config.action_mailer.perform_deliveries = true
+
+  # отправка почты по протоколу SMTP
+  config.action_mailer.delivery_method = :mailjet
+
+  # Настройки для Sendgrid
   # config.action_mailer.smtp_settings = {
   #   address: 'smtp.gmail.com',
   #   port: '587',
