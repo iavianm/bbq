27898cfb7b5ee90b0790f555381cfc3150d69bdd
iavianm <iavianm@mail.ru>
2022-01-25 13:34:16 +0300

    refactor event policy spec

--- spec/policies/event_policy_spec.rb
+++ spec/policies/event_policy_spec.rb
@@ -5,10 +5,19 @@ RSpec.describe EventPolicy, type: :policy do
   let(:event_creator) { FactoryBot.create(:user) }
 
   let(:event) { FactoryBot.create(:event, user: event_creator) }
+  let(:secure_event) { FactoryBot.create(:event, user: event_creator, pincode: '000') }
+
+  let(:event_creator_access) { PunditUser.new(event_creator, {}) }
+  let(:user_with_access) { PunditUser.new(user, cookies) }
+  let(:user_without_access) { PunditUser.new(user, {}) }
+  let(:anon_with_access) { PunditUser.new(nil, cookies) }
+  let(:anon_without_access) { PunditUser.new(nil, {}) }
+
+  let(:cookies) { { "events_#{secure_event.id}_pincode" => '000' } }
 
   subject { EventPolicy }
 
-  context 'when the user watches the event' do
+  describe 'when the user watches the event' do
     context 'user is event creator' do
       permissions :show? do
         it 'give access to event' do
@@ -34,11 +43,53 @@ RSpec.describe EventPolicy, type: :policy do
     end
   end
 
-  context 'when user edit, update, delete event' do
+  describe 'when the user watches the secure_event' do
+    context 'user is event creator' do
+      permissions :show? do
+        it 'give access to event' do
+          expect(subject).to permit(event_creator_access, secure_event)
+        end
+      end
+    end
+
+    context 'user without access' do
+      permissions :show? do
+        it 'give access to event' do
+          expect(subject).not_to permit(user_without_access, secure_event)
+        end
+      end
+    end
+
+    context 'user with access' do
+      permissions :show? do
+        it 'give access to event' do
+          expect(subject).to permit(user_with_access, secure_event)
+        end
+      end
+    end
+
+    context 'anon without access' do
+      permissions :show? do
+        it 'give access to event' do
+          expect(subject).not_to permit(anon_without_access, secure_event)
+        end
+      end
+    end
+
+    context 'anon with access' do
+      permissions :show? do
+        it 'give access to event' do
+          expect(subject).to permit(anon_with_access, secure_event)
+        end
+      end
+    end
+  end
+
+  describe 'when user edit, update, delete event' do
     context 'user is event creator' do
       permissions :edit?, :update?, :destroy? do
         it 'give access to event' do
-          expect(subject).to permit(event_creator, event)
+          expect(subject).to permit(event_creator_access, secure_event)
         end
       end
     end
@@ -46,7 +97,7 @@ RSpec.describe EventPolicy, type: :policy do
     context 'user is not event creator' do
       permissions :edit?, :update?, :destroy? do
         it 'not give access to event' do
-          expect(subject).not_to permit(user, event)
+          expect(subject).not_to permit(user_with_access, secure_event)
         end
       end
     end
@@ -54,7 +105,7 @@ RSpec.describe EventPolicy, type: :policy do
     context 'user is anon' do
       permissions :edit?, :update?, :destroy? do
         it 'not give access to event' do
-          expect(subject).not_to permit(nil, event)
+          expect(subject).not_to permit(anon_with_access, secure_event)
         end
       end
     end
