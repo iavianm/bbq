bbc40c4e866050cd9ca3972a70405cf1286207e6
iavianm <iavianm@mail.ru>
2022-01-26 21:38:00 +0300

    refactor resque photos and comments

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -59,15 +59,4 @@ class PhotosController < ApplicationController
   def photo_params
     params.fetch(:photo, {}).permit(:photo)
   end
-
-  def notify_subscribers_photo(event, photo)
-    # собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
-    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [photo.user&.email]).uniq
-
-    # XXX: Этот метод может выполняться долго из-за большого числа подписчиков
-    # поэтому в реальных приложениях такие вещи надо выносить в background задачи!
-    all_emails.each do |mail|
-      EventMailer.photo(event, photo, mail).deliver_now
-    end
-  end
 end
