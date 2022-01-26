06a865b2b4550a5b18d57da2375c53d820fd4b86
iavianm <iavianm@mail.ru>
2022-01-26 18:40:35 +0300

    resque ready setup

---
+++ app/jobs/event_email_notification_job.rb
@@ -0,0 +1,17 @@
+class EventEmailNotificationJob < ApplicationJob
+  queue_as :default
+
+  def perform(object)
+    event = object.event
+
+    all_emails =
+      (event.subscriptions.map(&:user_email) + [event.user.email]).uniq - [object&.user&.email]
+
+    case object
+    when Comment
+      all_emails.each { |mail| EventMailer.comment(event, object, mail).deliver_later }
+    when Photo
+      all_emails.each { |mail| EventMailer.photo(event, object, mail).deliver_later }
+    end
+  end
+end
