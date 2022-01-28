0626ce163866a49ec10cfdf9af88afdabb141893
iavianm <iavianm@mail.ru>
2022-01-28 15:07:20 +0300

    fix omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,28 @@
 class User < ApplicationRecord
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth(auth)
+    user = User.find_by('email = ?', auth['info']['email'])
+    if user.blank?
+      user = User.new(
+        {
+          provider: auth.provider,
+          uid: auth.uid,
+          email: auth.info.email,
+          password: Devise.friendly_token[0,20]
+        }
+      )
+      user.save!
+    end
+    user
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -24,10 +48,6 @@ class User < ApplicationRecord
 
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
-  end
-
-  def self.find_for_facebook_oauth(access_token)
-    byebug
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_090602) do
+ActiveRecord::Schema.define(version: 2022_01_28_113841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2022_01_27_090602) do
     t.string "avatar"
     t.string "provider"
     t.string "url"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
